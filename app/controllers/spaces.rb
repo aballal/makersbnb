a7584e71ad8b063664c1a86c40ab254e82093ed8
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-08 14:55:44 +0100

    Changed post route for spaces/new.erb, added post /spaces

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -1,14 +1,22 @@
 # Controller for spaces path
 class MakersBnB < Sinatra::Base
+
+  get '/spaces' do
+    @spaces = Space.all
+    # erb :'spaces/index' # This forms should get back to post /spaces route
+  end
+
+  post '/spaces' do
+    session[:available_from] = params[:available_from]
+    session[:available_to] = params[:available_to]
+    redirect '/spaces'
+  end
+
   get '/spaces/new' do
     erb :'spaces/new'
   end
 
-  get '/spaces' do
-    # erb :'spaces/index'
-  end
-
-  post '/spaces' do
+  post '/spaces/new' do
     Space.create(name: params[:name], description: params[:description],
                  price_per_night: params[:price_per_night],
                  available_from: params[:available_from],
