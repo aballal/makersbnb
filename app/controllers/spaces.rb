52f9793594e058cc76c997f03636943efdb15723
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-08-08 12:17:07 +0100

    Added get spaces route

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -4,11 +4,15 @@ class MakersBnB < Sinatra::Base
     erb :'spaces/new'
   end
 
+  get '/spaces' do
+    # erb :'spaces/index'
+  end
+
   post '/spaces' do
     Space.create(name: params[:name], description: params[:description],
                  price_per_night: params[:price_per_night],
                  available_from: params[:available_from],
                  available_to: params[:available_to])
-    # redirect here
+    redirect '/spaces'
   end
 end
