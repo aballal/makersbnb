3e79fde612fa9bd5aa709822db45fa3424e72826
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-08 09:56:50 +0100

    Saved a listing to the database

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -3,4 +3,12 @@ class MakersBnB < Sinatra::Base
   get '/spaces/new' do
     erb :'spaces/new'
   end
+
+  post '/spaces' do
+    Space.create(name: params[:name], description: params[:description],
+                 price_per_night: params[:price_per_night],
+                 available_from: params[:available_from],
+                 available_to: params[:available_to])
+    # redirect here
+  end
 end
--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,8 @@
 # Server
 class MakersBnB < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     redirect 'spaces/new'
   end
