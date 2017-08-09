f997360979c59ea6e32b1c96d18d589d8c09a722
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-09 18:13:58 +0100

    Added form that sends the selected date back to controller, added basic styling

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -2,6 +2,11 @@
 class MakersBnB < Sinatra::Base
 
   get '/bookings' do
+    @space = Space.first #For testing only. This needs to be changed to the selected space.
     erb :'bookings/new'
+  end
+
+  post '/bookings' do
+    redirect '/spaces'
   end
 end
