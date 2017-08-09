02e7aa491ec301a1ac9e3b6df5c2b680bc8d553a
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-09 07:20:40 +0100

    Updated feature test for listing a space to check each table column for the row and each field on the page

--- spec/features/list_space_spec.rb
+++ spec/features/list_space_spec.rb
@@ -1,10 +1,25 @@
 feature 'Listing a space' do
-  scenario 'I can list my space' do
-    visit('spaces/new')
-    expect(page.status_code).to eq 200
+  before do
+    navigate_to_list_a_space
+  end
+
+  scenario 'A user can navigate to List a Space' do
+    expect(current_path).to eq '/spaces/new'
     expect(page).to have_content('List a Space')
+  end
+
+  scenario 'A user can list a space with name, description, price per night,
+            available from date and available to date' do
+    # Check table count and each column for the added row
     expect { list_space }.to change(Space, :count).by(1)
+    space = Space.last
+    expect(space.name).to eq("Maya's Inn")
+    expect(space.description).to eq('Spacious BnB in the heart of the city')
+    expect(space.price_per_night).to eq(90)
+    expect(space.available_from).to eq(Date.parse('01-08-2017'))
+    expect(space.available_to).to eq(Date.parse('31-08-2017'))
+    # Check page and each field for each space
     expect(current_path).to eq '/spaces'
-    expect(page).to have_content("Maya's Inn")
+    expect(page).to have_content(page_content(Space.all))
   end
 end
--- spec/helpers/space.rb
+++ spec/helpers/space.rb
@@ -1,4 +1,9 @@
 module SpaceHelpers
+  def navigate_to_list_a_space
+    visit('/')
+    click_on 'List a Space'
+  end
+
   def list_space(name: "Maya's Inn",
                  description: 'Spacious BnB in the heart of the city',
                  price_per_night: '90',
@@ -11,5 +16,16 @@ module SpaceHelpers
     fill_in 'available_from', with: available_from
     fill_in 'available_to', with: available_to
     click_button 'List my Space'
+  end
+
+  def page_content(spaces)
+    page_content = spaces.map do |space|
+      "#{space.name}\n
+      #{space.description}\n
+      Price per Night: £#{'%.2f' % space.price_per_night} |
+      Available From: #{Date.parse(space.available_from.to_s).strftime('%d-%b-%Y')} |
+      Available To: #{Date.parse(space.available_to.to_s).strftime('%d-%b-%Y')}"
+    end
+    page_content.join(' ')
   end
 end
