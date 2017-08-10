2586550096f125cfd6a3f33b9969fccfb6d0d5f0
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-11 00:00:22 +0100

    Improved feature test for booking request

--- spec/features/book_request_spec.rb
+++ spec/features/book_request_spec.rb
@@ -5,9 +5,15 @@ feature 'Booking request' do
 
   scenario 'I can navigate to Book a Space' do
     expect(page.status_code).to eq 200
+    expect(page).to have_content('Book a Night')
   end
 
   scenario 'I can select a night and request to book' do
     expect { request_to_book }.to change(Booking, :count).by(1)
+    booking = Booking.last
+    expect(booking.night).to eq Date.parse('30-08-2017')
+    expect(booking.status).to eq 'Requested'
+    expect(booking.user_id).to eq User.first(email_address: 'john@example.com').id
+    expect(booking.space.name).to eq "Maya's Inn"
   end
 end
--- spec/helpers/booking.rb
+++ spec/helpers/booking.rb
@@ -2,6 +2,7 @@ module BookingHelper
   def navigate_to_booking
     sign_up
     list_space
+    sign_up(email_address: 'john@example.com')
     click_on 'Book'
   end
 
